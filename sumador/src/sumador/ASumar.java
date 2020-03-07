e374877f69f36790e747d7088753116e55174925
Juan Antonio Marco <jmarcoprojects@gmail.com>
2020-03-07 20:20:20 +0100

    primera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,33 @@
 package sumador;
 
+/**
+ * Clase para sumar todos los dígitos que componen un número
+ * @author J M
+ * @version: 1.0
+ */
+
 public class ASumar {
+	/**
+	 *Constructor
+	 */
+	public ASumar() {
+		}
+	
+	/**
+	 * Método que muestra el resultado de la suma de todos los dígitos que componen un número
+	 * @param cad contiene el número que se va a sumar
+	 * @return devuelve una cadena que muestra el número introducido y el resultado de la suma de sus dígitos
+	 */
+	public String mostrar(String cad) {
+        String numero = cad;
+        if(numero.length() == 1) {
+        	return (numero + " = " + numero);
+        	}
+        return numero;
+
+}
+
+
+	
 
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import org.junit.Test;
+/**
+ * Clase JUnit para los test de pruebas
+ * @author J M
+ * @version: 1.0
+ * 
+ */
 public class TestASumar {
-
+	/**
+	 * Test para un número de un sólo dígito
+	 */
 	@Test
-	public void test() {
-		fail("No implementado aun");
-	}
+	public void testUnaCifra() {
+        ASumar sumi = new ASumar();
+        String resultado = sumi.mostrar("5");
+        assertEquals("5 = 5",resultado );
+    }
 
 }
