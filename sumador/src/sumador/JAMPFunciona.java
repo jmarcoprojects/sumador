d6f3c962f01fbda84ed44ac6a27244e4e65060f7
Juan Antonio Marco <jmarcoprojects@gmail.com>
2020-03-07 21:42:47 +0100

    final

---
+++ sumador/src/sumador/JAMPFunciona.java
@@ -0,0 +1,12 @@
+package sumador;
+
+public class JAMPFunciona {
+
+	public static void main(String[] args) {
+		ASumar s = new ASumar();
+		System.out.println(s.mostrar("5"));
+		System.out.println(s.mostrar("-5"));
+		System.out.println(s.mostrar("1234"));
+	}
+
+}
